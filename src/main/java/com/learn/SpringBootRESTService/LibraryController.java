4c301575e3796d15333c9fa48161d52325dbc1fa
suman_mathad <suman.mathad@gmail.com>
2023-06-20 08:25:43 +0530

    Add book controller changes added

--- src/main/java/com/learn/SpringBootRESTService/LibraryController.java
+++ src/main/java/com/learn/SpringBootRESTService/LibraryController.java
@@ -1,15 +1,21 @@
 package com.learn.SpringBootRESTService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learn.SpringBootRESTService.repository.LibraryRepository;
+
 @RestController
 public class LibraryController {
 
+	@Autowired
+	LibraryRepository repository;
 	
 	@PostMapping("/addBook")
 	public void addBookImplementation(@RequestBody Library library) {
-		
+		library.setId(library.getIsbn()+library.getAisle());
+		repository.save(library);
 	}
 }
